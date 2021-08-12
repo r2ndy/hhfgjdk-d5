c797b95b50fcb87f9df1a1b650fb390cdeac5c19
r2ndy <88577982+r2ndy@users.noreply.github.com>
2021-08-12 17:38:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,8 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-//
-var prefix = '*'; // البريفكس الي تبيه
-var owners = '749064659457409106';  // حط ايديك
-//
-
-client.on('ready', () => {
-    client.user.setActivity(`${prefix}help.`, {
-        
-        type: 'PLAYING'// الحاله الي تبيها
-    }); 
-});
-
-
-//////////////////
+//////////////
+const prefix = '#'
   client.on('message', message => {
     if(message.author.bot) return;
             if (!points[message.author.id]) points[message.author.id] = {
@@ -431,7 +419,8 @@ if (message.content.startsWith(prefix + 'top')) {
    
   	message.channel.sendEmbed(prefixlor)
 }
-  });
+  
+});
 
 client.on('message', message => {
       if(message.author.bot) return;
@@ -444,6 +433,18 @@ if (message.content.startsWith(prefix + 'نقاطي')) {
 	.setDescription(`نقاطك: \`${userData.points}\``)
 	message.channel.sendEmbed(embed)
   }
+});
+client.on('message', message => {
+  if(message.author.bot) return;
+if (message.content.startsWith(prefix + 'points')) {
+if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+let userData = points[message.author.id];
+let embed = new Discord.RichEmbed()
+.setAuthor(`${message.author.tag}`, message.author.avatarURL)
+.setColor('#000000')
+.setDescription(`نقاطك: \`${userData.points}\``)
+message.channel.sendEmbed(embed)
+}
 });
 
 
@@ -537,7 +538,7 @@ client.on('message' , message => {
  });  
 
 client.on("message", function(message) {
-    var prefix = "*";
+    var prefix = "#";
    if(message.content.startsWith(prefix + "rps")) {
     let messageArgs = message.content.split(" ").slice(1).join(" ");
     let messageRPS = message.content.split(" ").slice(2).join(" ");
@@ -576,6 +577,54 @@ reaction3.on("collect", r => {
     })
 }
 });
+const devs = ["429972030092476437"]
+
+const adminprefix = "#";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+    if (message.content === (adminprefix + "Percie")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else     
+    if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`**${argresult}** : Done :>`)
+  return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+  } else
+    if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+        } else     
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  }
+    if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
+        console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log(`⚠️ Bot restarting... ⚠️`);
+        console.log("===============================================\n\n");
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(`Bot Successfully Restarted`);
+    }
+  
+  });
+
 
 
 
