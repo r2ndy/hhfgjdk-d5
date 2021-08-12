22c076b68b95be8e7c8780991629970242b39bf7
r2ndy <88577982+r2ndy@users.noreply.github.com>
2021-08-12 11:09:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,3 @@
-const express = require('express');
-const app = express();
-
-app.listen(() => console.log('Hi.'));
-
-app.use('/ping', (req, res) => {
-    res.send(new Date());
-});
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
