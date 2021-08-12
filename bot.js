6bca118ccb9029a40c5d3b5ad0e2d42853c84888
r2ndy <88577982+r2ndy@users.noreply.github.com>
2021-08-12 06:55:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,32 @@
+const express = require('express');
+const app = express();
+
+app.listen(() => console.log('Server started'));
+
+app.use('/ping', (req, res) => {
+    res.send(new Date());
+});
+require('events').EventEmitter.defaultMaxListeners = 400;
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs"); 
-const Canvas = require("canvas");
+const cmd = require('node-cmd');
+const ms = require('ms');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const canvas = require('canvas');
+const convert = require('hh-mm-ss');
+const fetchVideoInfo = require('youtube-info');
+const simpleytapi = require('simple-youtube-api');
+const util = require('util');
+const guild = require('guild');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const pretty = require('pretty-ms');
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+
 const jimp = require("jimp");
    let points = {}
    
